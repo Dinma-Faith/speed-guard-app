8532dbb69c226e9ae59935552a42b48fd59de4c7
Dinma Faith <phaytep@gmail.com>
2025-03-14 14:15:35 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import SpeedGuard from "./components/SpeedGuard"
+
+function App() {
+
+  return (
+      <div>
+        <SpeedGuard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SpeedGuard.tsx
@@ -0,0 +1,9 @@
+const SpeedGuard = () => {
+    return (
+        <div>
+            <h1>Speed Guard App</h1>
+        </div>
+    )
+}
+
+export default SpeedGuard;
